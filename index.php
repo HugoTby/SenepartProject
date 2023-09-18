ebc42885b0133868a7050921c564257fb7f3cbf3
fbotel17 <faustbtl17@gmail.com>
2023-09-18 16:41:58 +0200

    css formulaire

--- index.php
+++ index.php
@@ -12,10 +12,31 @@ include("class/User.php");
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!--===============================================================================================-->
 </head>
 
 <body>
@@ -26,32 +47,94 @@ include("class/User.php");
 
     if (isset($_POST["envoi"])) {
         $User->seConnecter($_POST["login"], $_POST["password"]);
-        if ($User->seConnecter($_POST["login"], $_POST["password"])) {
+        // if ($User->seConnecter($_POST["login"], $_POST["password"])) {
 
 
-            echo "bien joué beau gosse";
-        } else {
-            echo "gay";
-        }
+        //     echo "bien joué beau gosse";
+        // } else {
+        //     echo "pas bien joué beau gosse";
+        // }
     }
 
     ?>
-    <div>
-        <form action="" method="post">
-            <div>
-                <label for="login">Nom d'utilisateur : </label>
-                <input name="login" type="text">
-            </div>
-            <div>
-                <label for="password">Mot de passe : </label>
-                <input name="password" type="password">
-            </div>
-            <div>
-                <input name="envoi" value="Connecter" type="submit">
-            </div>
-        </form>
+    <!-- <div>
+        <div>
+            <label for="login">Nom d'utilisateur : </label>
+            <input name="login" type="text">
+        </div>
+        <div>
+            <label for="password">Mot de passe : </label>
+            <input name="password" type="password">
+        </div>
+        <div>
+            <input name="envoi" value="Connecter" type="submit">
+        </div>
 
-    </div>
+    </div> -->
+
+    <form action="" method="post">
+
+        <div class="limiter">
+            <div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+                <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+                    <form class="login100-form validate-form">
+                        <span class="login100-form-title p-b-49">
+                            Login
+                        </span>
+
+                        <div class="wrap-input100 validate-input m-b-23" data-validate="Username is reauired">
+                            <span class="label-input100">Nom d'utilisateur :</span>
+                            <input class="input100" type="text" name="login" placeholder="Type your username">
+                            <span class="focus-input100" data-symbol="&#xf206;"></span>
+                        </div>
+
+                        <div class="wrap-input100 validate-input" data-validate="Password is required">
+                            <span class="label-input100">Mot de passe : </span>
+                            <input class="input100" type="password" name="password" placeholder="Type your password">
+                            <span class="focus-input100" data-symbol="&#xf190;"></span>
+                        </div>
+
+                        <div class="text-right p-t-8 p-b-31">
+                        </div>
+
+                        <div class="container-login100-form-btn">
+                            <div class="wrap-login100-form-btn">
+                                <div class="login100-form-bgbtn"></div>
+                                <button name="envoi" type="submit" class="login100-form-btn">
+                                    Login
+                                </button>
+
+                            </div>
+                        </div>
+
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </form>
+
+
+
+    <div id="dropDownSelect1"></div>
+
+
+    <!--===============================================================================================-->
+    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/animsition/js/animsition.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/bootstrap/js/popper.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/daterangepicker/moment.min.js"></script>
+    <script src="vendor/daterangepicker/daterangepicker.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/countdowntime/countdowntime.js"></script>
+    <!--===============================================================================================-->
+    <script src="js/main.js"></script>
 </body>
 
 </html>
